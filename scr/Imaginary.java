460ff252b0d686010d0a039aecf9d9fec4f769a5
Mo-Mans0ur <Moha1998@outlook.dk>
2021-09-09 09:07:01 +0200

    comment

--- scr/Imaginary.java
+++ scr/Imaginary.java
@@ -1,6 +1,8 @@
 public class Imaginary {
     public static void main(String[] args) {
         System.out.println("Fuck this shit!");
+
+        System.out.println("animated-disco is the best");
     }
 }
 
